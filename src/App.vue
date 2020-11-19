22abe8c97470b581b5b72e1269cd2bcb0bc6779b
akin <jia.por911@qq.com>
2020-11-19 21:37:26 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+   app根组件
+  </div>
+</template>
+<script>
+export default {
+  name: 'app'
+
+}
+</script>
+
+<style>
+  div {
+    width: 100px;
+    height: 100px;
+  }
+</style>
